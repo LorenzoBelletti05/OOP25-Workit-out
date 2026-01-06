30b8181fa4ed9a494c47215a8704c4c91715b3ef
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-06 21:25:43 +0100

    Added classes linked to the contracts

---
+++ src/main/java/it/unibo/workitout/model/workout/impl/PlannedExerciseImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.workitout.model.workout.impl;
+
+import it.unibo.workitout.model.workout.contracts.PlannedExercise;
+
+public class PlannedExerciseImpl implements PlannedExercise {
+    
+}
