ab4da63ffd3744f61f497de0bf63250584c8ce1e
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-06 21:55:16 +0100

    Added AttitudeExercise.java enum and added in Exercise.java a private field set which use the enum

---
+++ src/main/java/it/unibo/workitout/model/workout/impl/AttitudeExercise.java
@@ -0,0 +1,8 @@
+package it.unibo.workitout.model.workout.impl;
+
+public enum AttitudeExercise {
+    WHEIGHT_LOSS,
+    MUSCLE_GAIN,
+    WHEIGHT_MAINTENANCE,
+    WHEIGHT_GAIN
+}
--- src/main/java/it/unibo/workitout/model/workout/impl/Exercise.java
+++ src/main/java/it/unibo/workitout/model/workout/impl/Exercise.java
@@ -1,13 +1,20 @@
 package it.unibo.workitout.model.workout.impl;
 
-public class Exercise {
+import java.util.Set;
+
+import it.unibo.workitout.model.workout.contracts.PlannedExercise;
+
+public class Exercise{
 
     private final String name;
     private final double calorieBurned;
+    private final Set<AttitudeExercise> exerciseMission;
+    
 
-    public Exercise(String name, double calorieBurned) {
+    public Exercise(String name, double calorieBurned, Set<AttitudeExercise> exerMission) {
         this.name = name;
         this.calorieBurned = calorieBurned;
+        this.exerciseMission = exerMission;
     }
 
     public String getName() {
