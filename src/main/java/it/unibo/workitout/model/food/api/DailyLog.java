425b70b60a72cc1184dc73f44b7a0181bcacabed
Leonardo Castiglione <leonardo.castiglione@studio.unibo.it>
2026-01-07 11:14:37 +0100

    Aggiornato DailyLog.java

--- src/main/java/it/unibo/workitout/model/food/api/DailyLog.java
+++ src/main/java/it/unibo/workitout/model/food/api/DailyLog.java
@@ -1,7 +1,20 @@
 package it.unibo.workitout.model.food.api;
 
-//interfaccia DailyLog
+import java.time.LocalDate;
+import java.util.Map;
+
 public interface DailyLog {
-    String getDate();
-    double getTotalCalories();
+    LocalDate getDate();
+
+    //Aggiunge un cibo e la sua quantità
+    void addFoodEntry(Food food, int grams);
+
+    //Dati totali della giornata
+    double getTotalKcal();
+    double getTotalProteins();
+    double getTotalCarbs();
+    double getTotalFats();
+
+    //Mappa dei cibi inseriti e le relative quantità
+    Map<Food, Integer> getFoodsConsumed();
 }
