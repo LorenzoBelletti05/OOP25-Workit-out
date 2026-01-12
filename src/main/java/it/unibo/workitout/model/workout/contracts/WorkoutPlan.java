21857bd5403e123c121f8c1a840867be53e4ecde
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-12 22:40:53 +0100

    Added definition of method in WorkoutPlan interface

--- src/main/java/it/unibo/workitout/model/workout/contracts/WorkoutPlan.java
+++ src/main/java/it/unibo/workitout/model/workout/contracts/WorkoutPlan.java
@@ -1,5 +1,9 @@
 package it.unibo.workitout.model.workout.contracts;
 
+import java.util.Set;
+
+import it.unibo.workitout.model.workout.impl.WorkoutSheet;
+
 /**
  * Interface that representing the entire workout for the user
  * based on his preferencies.
@@ -10,5 +14,50 @@ package it.unibo.workitout.model.workout.contracts;
  * </p>
  */
 public interface WorkoutPlan {
+
+    /**
+     * Return the hole exercise in a session.
+     * 
+     * @return an unmodifiable set of all session.
+     */
+    Set<WorkoutSheet> getSheets();
+
+    /**
+     * Utility methot that return all the exercises from each sheets.
+     * 
+     * @return the planned exercises in the plan.
+     */
+    Set<PlannedExercise> getAllExercise();
+
+    /**
+     * Calculate and return the total calories for the plan.
+     * 
+     * @return sum of all the burned calories from all exercises.
+     */
+    double getTotalBurnedCalories();
+
+    /**
+     * Return all the filter plan based on strenght.
+     * 
+     * @return the plan based on strenght.
+     */
+    Set<StrengthPlannedExercise> getStrenghtExercise();
+
+    /**
+     * Return all the filter plan based on cardio.
+     * 
+     * @return the plan based on cardio.
+     */
+    Set<CardioPlannedExercise> getCardiotExercise();
+
+
+    /**
+     * It aggregates the volume training only for strnght exercise.
+     * 
+     * @return total volume of exercise in the plan.
+     */
+    double getTotalStrenghtVolume();
+
+
 
 }
