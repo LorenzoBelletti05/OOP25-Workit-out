e53868bf28dda3de48978d7fcdecc9dd6bbe3bd7
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-12 22:17:14 +0100

    Modified PlannedExerciseImpl in an abstract class

--- src/main/java/it/unibo/workitout/model/workout/impl/PlannedExerciseImpl.java
+++ src/main/java/it/unibo/workitout/model/workout/impl/PlannedExerciseImpl.java
@@ -7,9 +7,12 @@ import it.unibo.workitout.model.workout.contracts.PlannedExercise;
  * 
  * <p>
  * It decorates the {@link Exercise} with specific training parameters
- * as a set, repetitions, and weight/intensity.
+ * as a set, repetitions, and weight/intensity subdivided by the two sub-interface
+ * from PlannedExercise.
+ * 
+ * The class will be abstract because it can implements or not the sub-interface.
  * </p>
  */
-public class PlannedExerciseImpl implements PlannedExercise {
+public abstract class PlannedExerciseImpl implements PlannedExercise {
 
 }
