8b51f86984e06f2e87e00c91d709efa10e5355e2
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-18 22:31:42 +0100

    Started implementing WorkoutPlanImpl

--- src/main/java/it/unibo/workitout/model/workout/impl/WorkoutPlanImpl.java
+++ src/main/java/it/unibo/workitout/model/workout/impl/WorkoutPlanImpl.java
@@ -1,23 +1,69 @@
 package it.unibo.workitout.model.workout.impl;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.ToDoubleFunction;
 
 import it.unibo.workitout.model.workout.contracts.CardioPlannedExercise;
 import it.unibo.workitout.model.workout.contracts.PlannedExercise;
 import it.unibo.workitout.model.workout.contracts.StrengthPlannedExercise;
 import it.unibo.workitout.model.workout.contracts.WorkoutPlan;
+import it.unibo.workitout.model.workout.contracts.WorkoutSheet;
+
+import java.time.LocalDate;
 
 /**
  * Implementation of {@link WorkoutPlan}.
  * 
  * <p>
- * This class aggregates multiples {@link WorkoutSheet} istence to represent the training program.
+ * This class aggregates multiples {@link WorkoutSheetImpl} istence to represent the training program.
  * </p>
  */
-public final class WorkoutPlanImpl implements WorkoutPlan {
+public final class WorkoutPlanImpl extends NameFunction implements WorkoutPlan {
+
+    private Map<LocalDate, List<WorkoutSheet>> workoutPlan;
+    
+    public WorkoutPlanImpl(final String namePlan) {
+        super(namePlan);
+    }
+
+    private double sumAll(ToDoubleFunction<PlannedExercise> sheetPlan) {
+        double sum = 0.0;
+        for (List<WorkoutSheet> sheetExercisesList : workoutPlan.values()) {
+            for (WorkoutSheet workoutSheet : sheetExercisesList) {
+                
+                sum+=sheetPlan.applyAsDouble((PlannedExercise) workoutSheet);
+            }
+            
+        }
+        return sum;
+    }
+
+    /**
+     * Public getter that return this unmoodifiable structure data.
+     * 
+     * @return the unmodifiable set of Workoutsheet.
+     */
+    public Map<LocalDate, List<WorkoutSheet>> getWorkoutSheet() {
+        return Collections.unmodifiableMap(this.workoutPlan);
+    }
 
     @Override
-    public Set<WorkoutSheet> getSheets() {
+    public double getVolume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBurnedCalories'");
+    }
+
+    @Override
+    public double getBurnedCalories() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBurnedCalories'");
+    }
+
+    @Override
+    public Set<WorkoutSheetImpl> getSheets() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getSheets'");
     }
@@ -26,12 +72,6 @@ public final class WorkoutPlanImpl implements WorkoutPlan {
     public Set<PlannedExercise> getAllExercise() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAllExercise'");
-    }
-
-    @Override
-    public double getTotalBurnedCalories() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTotalBurnedCalories'");
     }
 
     @Override
@@ -44,12 +84,6 @@ public final class WorkoutPlanImpl implements WorkoutPlan {
     public Set<CardioPlannedExercise> getCardiotExercise() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getCardiotExercise'");
-    }
-
-    @Override
-    public double getTotalStrenghtVolume() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTotalStrenghtVolume'");
-    }
+    }    
 
 }
