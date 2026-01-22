43d976dfe48da3b8f6c408da5ba30976f214341e
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-22 13:13:11 +0100

    Added file to create the logic that calculate the workout based on data gived from user

---
+++ src/main/java/it/unibo/workitout/model/workout/impl/WorkoutCreatorImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.workitout.model.workout.impl;
+
+import it.unibo.workitout.model.workout.contracts.WorkoutCreator;
+
+public class WorkoutCreatorImpl implements WorkoutCreator{
+    
+}
