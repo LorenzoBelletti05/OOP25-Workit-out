2822a18417ab910f5332922f90eb8afd691bfb10
Lorenzo Belletti <lorenzo.belletti5@studio.unibo.it>
2026-01-31 10:25:17 +0100

    Modify method to read correctly a value from the json exercise.json

--- src/main/java/it/unibo/workitout/model/main/dataManipulation/loadSaveData.java
+++ src/main/java/it/unibo/workitout/model/main/dataManipulation/loadSaveData.java
@@ -3,22 +3,14 @@ package it.unibo.workitout.model.main.dataManipulation;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Serializable;
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
 
 import it.unibo.workitout.model.main.WorkoutUserData;
 import it.unibo.workitout.model.workout.contracts.WorkoutPlan;
-import it.unibo.workitout.model.workout.impl.Exercise;
 import it.unibo.workitout.model.workout.impl.WorkoutPlanImpl;
 
 public class loadSaveData {
